9c6065a0ae35f42143fd7094721efce512f10cd9
pandalv <pandalv@foxmail.com>
2017-02-14 13:24:05 +0800

    change app port

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('/timestamp/:time', function (req, res) {
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-app.listen(8080, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('Example app listening on port 8080!')
 })
